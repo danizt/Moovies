12dc19a74dfd6db5afb857356324064f6b196c30
xAceV <danizt96@gmail.com>
2017-05-05 13:47:00 +0200

    Contenedor de Fragments funcionando

---
+++ app/src/main/java/com/acev/moovies/FragmentPopulares.java
@@ -0,0 +1,32 @@
+package com.acev.moovies;
+
+
+import android.os.Bundle;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Daniel on 05/05/2017.
+ */
+
+public class FragmentPopulares extends Fragment {
+    public FragmentPopulares() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_populares, container, false);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
