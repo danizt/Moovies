5ce1b3c8bb49888038b0802ee9afa55947c4092d
Ace <danizt96@gmail.com>
2017-05-24 15:16:58 +0200

    cv0 y cv1 de FrDetalle

--- app/src/main/java/com/acev/moovies/Fragments/FrDetalle.java
+++ app/src/main/java/com/acev/moovies/Fragments/FrDetalle.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -63,26 +64,10 @@ public class FrDetalle extends Fragment {
                 }
 
                 // Datos recibidos
-                //Log.d("backdrop", det.getBackdrop());
                 Log.d("adult", det.getAdult().toString());
                 Log.d("overview", det.getOverview());
                 Log.d("poster", det.getPoster());
-                Log.d("release_date", det.getRelease_date());
-                Log.d("title", det.getTitle());
                 Log.d("vote_average", det.getVote_average());
-                Log.d("original_title", det.getOriginal_title());
-                Log.d("tagline", det.getTagline());
-
-                List<String> genres = det.getGenres();
-                for(int e = 0; e < genres.size(); e++){
-                    Log.d("genre " + (e+1), genres.get(e));
-                }
-
-                List<String> production_companies = det.getProduction_company();
-                for(int f = 0; f < production_companies.size(); f++){
-                    Log.d("production_companies " + (f+1), production_companies.get(f));
-                }
-
                 Log.d("trailer", det.getTrailer());
 
                 // Título de la toolbar
@@ -94,37 +79,80 @@ public class FrDetalle extends Fragment {
                 ivTitulo.setScaleType(ImageView.ScaleType.FIT_XY);
                 Picasso.with(getActivity()).load(API_POSTER_URL.replace("<IMG_PATH>", det.getBackdrop())).into(ivTitulo);
 
-//                // Título
-//                TextView tvTitulo = (TextView) getView().findViewById(R.id.tvdtTitulo);
-//                tvTitulo.setText(det.getTitle());
-//
-//                // Fecha de salida
-//                String formattedDate = null;
-//                try {
-//                    Date date = new SimpleDateFormat("yyyy-MM-dd").parse(det.getRelease_date());
-//                    formattedDate = new SimpleDateFormat("dd-MM-yyyy").format(date);
-//                } catch (ParseException e) {
-//                    e.printStackTrace();
-//                }
-//                TextView tvRelease = (TextView) getView().findViewById(R.id.tvdtReleaseDate);
-//                tvRelease.setText(formattedDate);
+
+                // ***************
+                // ***** cv0 *****
+                // ***************
+
+
+                // Titulo Original
+
+                if (!det.getTagline().equals("")){
+                    TextView tvTagline = (TextView) getView().findViewById(R.id.tvdtTagline);
+                    tvTagline.setText("\""+det.getTagline()+"\"");
+                }else{
+                    CardView cv0 = (CardView) getView().findViewById(R.id.cv0);
+                    cv0.setVisibility(View.GONE);
+
+                }
+
+
+                // ***************
+                // ***** cv1 *****
+                // ***************
+
+
+                // Titulo Original
+                TextView tvTituloO = (TextView) getView().findViewById(R.id.tvdtOriginalT);
+                tvTituloO.setText(det.getOriginal_title());
+
+
+                // Fecha de salida
+                String formattedDate = null;
+                try {
+                    Date date = new SimpleDateFormat("yyyy-MM-dd").parse(det.getRelease_date());
+                    formattedDate = new SimpleDateFormat("dd-MM-yyyy").format(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                TextView tvRelease = (TextView) getView().findViewById(R.id.tvdtRelease);
+                tvRelease.setText(formattedDate);
+
+
+                // Género
+                String genre = "";
+                TextView tvGenre = (TextView) getView().findViewById(R.id.tvdtGenre);
+                List<String> genres = det.getGenres();
+                for (int e = 0; e < genres.size(); e++) {
+                    if (e > 0) {
+                        genre = genre + ", " + genres.get(e);
+                    } else {
+                        genre = genres.get(e);
+                    }
+                }
+                tvGenre.setText(genre);
+
+
+                // Productoras
+                String prod = "";
+                TextView tvProd = (TextView) getView().findViewById(R.id.tvdtProd);
+                List<String> production_companies = det.getProduction_company();
+                for (int e = 0; e < production_companies.size(); e++) {
+                    if (e > 0) {
+                        prod = prod + ", " + production_companies.get(e);
+                    } else {
+                        prod = production_companies.get(e);
+                    }
+                }
+                tvProd.setText(prod);
+
 //
 //                // Nota media
 //                TextView tvAverage = (TextView) getView().findViewById(R.id.tvdtVote);
 //                tvAverage.setText(det.getVote_average());
 //
-//                // Género
-//                String genre = "";
-//                TextView tvGenre = (TextView) getView().findViewById(R.id.tvdtGenre);
-//                List<String> genres = det.getGenres();
-//                for(int e = 0; e < genres.size(); e++){
-//                    if (e>0){
-//                        genre = genre + ", " + genres.get(e);
-//                    }else{
-//                        genre = genres.get(e);
-//                    }
-//                }
-//                tvGenre.setText(genre);
+
 
             }
         }.execute();
@@ -133,11 +161,12 @@ public class FrDetalle extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
     }
+
     @Override
     public void onStop() {
         super.onStop();
-        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().show();
     }
 }
